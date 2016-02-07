de9babc3d5cb271a06099ff58ca501397f083804
kjh <kalle.hiljanen@helsinki.fi>
2016-02-07 16:55:03 +0200

    viikko 2 loput

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,8 @@
 class Brewery < ActiveRecord::Base
+  include RatingAverage
+  
   has_many :beers
+  has_many :ratings, through: :beers
   
   def to_s
     "#{self.name}"
---
+++ app/models/concerns/rating_average.rb
@@ -0,0 +1,7 @@
+module RatingAverage
+  extend ActiveSupport::Concern
+
+  def rating_average
+    self.ratings.average(:score).to_f 
+  end
+end
